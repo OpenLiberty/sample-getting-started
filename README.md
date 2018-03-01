ebcc4bbe54f3309596a0c2771c0a32475333092d
cbridgha <cbridgha@us.ibm.com>
2018-03-01 15:49:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![](https://github.com/OpenLiberty/open-liberty/blob/master/logos/logo_horizontal_light_navy.png)
+
+
---
+++ src/test/java/it/io/openliberty/sample/health/HealthTest.java
@@ -0,0 +1,68 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::comment[]
+// tag::HealthTest[]
+package it.io.openliberty.sample.health;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import javax.json.JsonArray;
+import org.junit.After;
+import org.junit.Test;
+
+public class HealthTest {
+
+  private JsonArray servicesStates;
+  private static HashMap<String, String> dataWhenServicesUP, dataWhenInventoryDown;
+
+  static {
+    dataWhenServicesUP = new HashMap<String, String>();
+    dataWhenInventoryDown = new HashMap<String, String>();
+
+    dataWhenServicesUP.put("SystemResource", "UP");
+    dataWhenServicesUP.put("InventoryResource", "UP");
+
+    dataWhenInventoryDown.put("SystemResource", "UP");
+    dataWhenInventoryDown.put("InventoryResource", "DOWN");
+  }
+
+  @Test
+  public void testIfServicesAreUp() {
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(200);
+    checkServicesStates(dataWhenServicesUP, servicesStates);
+  }
+
+  @Test
+  public void testIfInventoryServiceIsDown() {
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(200);
+    checkServicesStates(dataWhenServicesUP, servicesStates);
+    HealthTestUtil.changeProperty(HealthTestUtil.INV_MAINTENANCE_FALSE, HealthTestUtil.INV_MAINTENANCE_TRUE);
+    servicesStates = HealthTestUtil.connectToHealthEnpoint(503);
+    checkServicesStates(dataWhenInventoryDown, servicesStates);
+  }
+
+  private void checkServicesStates(HashMap<String, String> testData, JsonArray servicesStates) {
+    testData.forEach((service, expectedState) -> {
+      assertEquals("The state of " + service + " service is not matching the ", expectedState,
+                   HealthTestUtil.getActualState(service, servicesStates));
+    });
+
+  }
+
+  @After
+  public void teardown() {
+    HealthTestUtil.cleanUp();
+  }
+
+}
+// end::HealthTest[]
