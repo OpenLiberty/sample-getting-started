54aa35226354a5a952c12e15737c184f258d3475
Hank W Ibell <hwibell@us.ibm.com>
2023-08-08 15:22:03 -0400

    Add dummy unit test

---
+++ src/test/java/it/io/openliberty/sample/BlankTest.java
@@ -0,0 +1,10 @@
+package it.io.openliberty.sample;
+
+import org.junit.jupiter.api.Test;
+
+public class BlankTest {
+    @Test
+    public void EmptyTest() {
+        System.out.println("Empty test ran!");
+    }
+}
