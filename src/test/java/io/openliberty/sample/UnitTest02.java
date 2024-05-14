93f5b6ff61b42aad388f939c614eeccdff97e776
Paul Bennett <pwbennet@us.ibm.com>
2024-05-14 10:52:14 -0500

    Add unit tests, sync with OpenLiberty/sample-getting started

---
+++ src/test/java/io/openliberty/sample/UnitTest02.java
@@ -0,0 +1,18 @@
+package io.openliberty.sample;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class UnitTest02 {
+
+    @Test
+    public void alwaysPass() {
+    }
+
+    @Ignore
+    @Test
+    public void alwaysSkip() {
+    }
+}
