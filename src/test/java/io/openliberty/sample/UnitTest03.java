7e8f94a1b938f96c7b473bae44d278eaffbcff84
Paul Bennett <pwbennet@us.ibm.com>
2024-06-07 10:10:07 -0500

    Add unit testsuite with only one test

---
+++ src/test/java/io/openliberty/sample/UnitTest03.java
@@ -0,0 +1,13 @@
+package io.openliberty.sample;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class UnitTest03 {
+
+    @Test
+    public void alwaysPass() {
+    }
+}
