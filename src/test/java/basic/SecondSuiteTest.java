4eb9ef1ff7809aa692bd1b5a1791697da23d9dde
Steven Zvonek <6392944+steven1046@users.noreply.github.com>
2024-09-16 09:00:35 -0500

    Try to get a failed build

---
+++ src/test/java/basic/SecondSuiteTest.java
@@ -0,0 +1,15 @@
+package basic.unittests;
+
+import org.junit.jupiter.api.Test;
+
+public class SecondSuiteTest {
+    @Test
+    public void secondTest1() {
+        System.out.println("secondTest1 successful!");
+    }
+
+    @Test
+    public void secondTest2() {
+        System.out.println("secondTest2 successful!");
+    }
+}
